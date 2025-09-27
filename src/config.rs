67302d9a4b19575be6e9b47576b3ba3536dd93ce
wanergou <50771267+wangegou@users.noreply.github.com>
2025-09-27 15:04:58 +0800

    改为自己的 ID/ 中继服务器

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["109.199.127.173"];
+pub const RS_PUB_KEY: &str = "dePWjPnvWAzGIx1tMhcszqsu9BkLzX8EZw0snZTPc78=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
